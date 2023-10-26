f2133aa63ebbb21462d6473567d651f2af90ece4
Marcin Bojara <marcin_5@mail.ru>
2023-10-26 11:07:40 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { App } from "App";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import "./index.css";
+import { store } from "./store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
