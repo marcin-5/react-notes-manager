61c3662324483885e9a4b80c43117e2b034328e0
Marcin Bojara <marcin_5@mail.ru>
2023-10-26 13:37:15 +0200

    setup react router and pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import { App } from "App";
+import { Note } from "pages/Note/Note";
+import { NoteBrowse } from "pages/NoteBrowse/NoteBrowse";
+import { NoteCreate } from "pages/NoteCreate/NoteCreate";
+import { PageNotFound } from "pages/PageNotFound/PageNotFound";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import { store } from "./store";
 
@@ -10,7 +15,16 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="/" element={<NoteBrowse />} />
+            <Route path="/note/:id" element={<Note />} />
+            <Route path="/note/new" element={<NoteCreate />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
