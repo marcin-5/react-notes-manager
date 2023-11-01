8217eac274a539a5b2b7c8bfdbea13907b86a769
Marcin Bojara <marcin_5@mail.ru>
2023-11-01 19:29:10 +0100

    add signin form structure

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,3 +1,22 @@
+import { ButtonPrimary } from "components/ButtonPrimary/ButtonPrimary";
+import { Link } from "react-router-dom";
+import s from "./style.module.css";
+
 export function Signin() {
-  return <>Signin</>;
+  const form = (
+    <div className={s.formContainer}>
+      <h2 className={s.title}>
+        Signin <br /> to access your team notes
+      </h2>
+      <form className={s.formGroup}>
+        <input type="text" />
+        <input type="text" />
+        <ButtonPrimary className={s.button}>Sign in!</ButtonPrimary>
+        <span>
+          Don't have an account yet? <Link>Signup</Link>
+        </span>
+      </form>
+    </div>
+  );
+  return <>{form}</>;
 }
