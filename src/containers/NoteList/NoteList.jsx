b1d7871830bab0d1fd5b9b103c50d00788beb5d6
Marcin Bojara <marcin_5@mail.ru>
2023-11-01 18:39:00 +0100

    add search bar component

--- src/containers/NoteList/NoteList.jsx
+++ src/containers/NoteList/NoteList.jsx
@@ -5,8 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { deleteNote } from "store/notes/notes-slice";
 import s from "./style.module.css";
 
-export function NoteList(props) {
-  const noteList = useSelector((store) => store.noteSlice.noteList);
+export function NoteList({ noteList }) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
