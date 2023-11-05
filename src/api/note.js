80bcb3c7ffe64a99997fd329be5dd645fa3eab51
Marcin Bojara <marcin_5@mail.ru>
2023-11-05 18:45:49 +0100

    create, update and delete note with firestore

--- src/api/note.js
+++ src/api/note.js
@@ -1,12 +1,36 @@
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  updateDoc,
+} from "firebase/firestore";
 import { FireBaseApp } from "utils/firebase";
 
 export class NoteAPI {
-  static async create(note) {}
+  static async create(formValues) {
+    const response = await addDoc(collection(FireBaseApp.db, "notes"), formValues);
+    return {
+      id: response.id,
+      ...formValues,
+    };
+  }
 
-  static async deleteById(noteId) {}
+  static async deleteById(noteId) {
+    deleteDoc(doc(FireBaseApp.db, "notes", noteId));
+  }
 
-  static async updateById(noteId, note) {}
+  static async updateById(id, values) {
+    const q = doc(FireBaseApp.db, "notes", id);
+    await updateDoc(q, values);
+    return {
+      id,
+      ...values,
+    };
+  }
 
   static async fetchAll() {
     const q = query(collection(FireBaseApp.db, "notes"), orderBy("created_at", "asc"));
