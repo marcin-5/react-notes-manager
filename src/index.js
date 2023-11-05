3b76a4954a9235ed9410f0dec21be52b99ae5e83
Marcin Bojara <marcin_5@mail.ru>
2023-11-05 11:51:36 +0100

    persist the redux auth slice

--- src/index.js
+++ src/index.js
@@ -9,9 +9,10 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 import { FireBaseApp } from "utils/firebase";
 import "./index.css";
-import { store } from "./store";
+import { persistor, store } from "./store";
 
 FireBaseApp.init();
 
@@ -20,18 +21,20 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/" element={<ProtectedApp />}>
-            <Route path="/" element={<NoteBrowse />} />
-            <Route path="/note/:noteId" element={<Note />} />
-            <Route path="/note/new" element={<NoteCreate />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={<ProtectedApp />}>
+              <Route path="/" element={<NoteBrowse />} />
+              <Route path="/note/:noteId" element={<Note />} />
+              <Route path="/note/new" element={<NoteCreate />} />
+              <Route path="*" element={<PageNotFound />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
